8918c9139bfacaeffe6d05c48dca68224e962e21
phatcharee_p <phatcharee.pu@gmail.com>
2026-01-03 14:58:18 +0700

    ลบ folder ที่ไม่ได้ใช้งานออกไป

--- src/versions/v.3/online-shop-inventory/backend/sql/002_seed.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO products (sku, name, price_cents, stock)
-VALUES
-  ('SKU-TSHIRT', 'T-Shirt', 39900, 10),
-  ('SKU-MUG',    'Coffee Mug', 25000, 4),
-  ('SKU-HAT',    'Cap / Hat', 19900, 7)
-ON CONFLICT (sku) DO NOTHING;
