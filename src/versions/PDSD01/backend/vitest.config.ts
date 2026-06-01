290c3be11b3f4ac05c3668119fbf61a610ea7a5b
Phatcharee <phatcharee.pu@gmail.com>
2026-06-01 14:44:59 +0700

    remove old version and ci update

--- src/versions/PDSD01/backend/vitest.config.ts
+++ src/versions/PDSD01/backend/vitest.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     setupFiles: ['tests/setup.ts'],
+    pool: 'forks',
+    poolOptions: {
+      forks: {
+        singleFork: true,
+      },
+    },
   },
 });
--- src/versions/SCSD01/sonarqube.md
+++
@@ -1,3 +0,0 @@
-./scan.sh ../src/versions/SCSD01 sqp_9b720497ebb7a376adfaada9af8da2b027758720
-
-![alt text](image-1.png)
